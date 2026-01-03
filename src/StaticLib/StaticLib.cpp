de79f54e3f917600fdc8e1e9b6be6e0d4fafc3ac
kokoyasa <krms0727@gmail.com>
2026-01-04 01:28:51 +0900

    Unit Test Passed
    テストを通しました。(成功6/6)

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,9 +2,27 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+#include <stdlib.h>   // qsort用
+// qsort用の比較関数
+// 'TEST_METHOD(たくさんの要素を入れ替え)'でタイムアウトするのを回避する
+static int item_cmp(const void *a, const void *b)
+{
+    const item * ia = (const item*)a;
+    const item * ib = (const item*)b;
+
+    return ia->key - ib->key;
+}
 
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    // 引数チェック
+    if (begin == NULL || end == NULL) return false;
+
+    if (begin >= end) return false;
+
+    size_t count = (size_t)(end - begin);
+    qsort(begin, count, sizeof(item), item_cmp);
+
+    return true;
 }
